5ed6124a89e010cae167964b13e27eb869e8f8e4
Muha-B20 <muhammeddcakmak@outlook.com>
2020-11-14 11:39:47 -0700

    updated hooks

--- src/test/java/com/vytrack/step_definitions/Hooks.java
+++ src/test/java/com/vytrack/step_definitions/Hooks.java
@@ -4,19 +4,19 @@ import com.vytrack.utilities.Driver;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.cucumber.java.Scenario;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 
 import java.util.concurrent.TimeUnit;
 
+/**
+ * Hooks name is not reserved. You may name this class in any way.
+ * For example: SuiteSetupAndTearDown
+ * Hooks triggered based on tags not class name or their location.
+ * These methods can be a part of any step definition class.
+ * Common practice is to store them in the separate class.
+ */
 public class Hooks {
-
-    /**
-     * Hooks name is not reserved. You may name this class in any way.
-     * For example: SuiteSetupAndTearDown
-     * Hooks triggered based on tags not class name or their location.
-     * These methods can be a part of any step definition class.
-     * Common practice is to store them in the separate class.
-     */
-
 
 //    hook before = @BeforeMethod in TestNG
 //    hook after = @AfterMethod in TestNG
@@ -25,13 +25,11 @@ public class Hooks {
 
     @Before
     public void setup(Scenario scenario) {
-        //   System.out.println(scenario.getSourceTagNames());
         System.out.println("::: Starting Automation:::");
         Driver.getDriver().manage().window().maximize();
         Driver.getDriver().manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-
     }
-
+//    this hook will run only before scenarios with a tag @db
 
     /**
      * @db Scenario: I don't know but here I'm connecting to DB
@@ -39,8 +37,6 @@ public class Hooks {
      * <p>
      * order = 0 - to define hooks execution order
      */
-
-    //    this hook will run only before scenarios with a tag @db
     @Before(value = "@db", order = 0)
     public void dbSetup() {
         System.out.println("::: Connecting to the database:::");
@@ -52,10 +48,15 @@ public class Hooks {
     }
 
     @After
-    public void tearDown() {
+    public void tearDown(Scenario scenario) {
         //close browser, close DB connection, close tunnel,capture screenshot of the error, etc..
         //this is a hook after
         //runs automatically after every test
+        if (scenario.isFailed()) {
+            byte[] data = ((TakesScreenshot) Driver.getDriver()).getScreenshotAs(OutputType.BYTES);
+            scenario.attach(data, "image/png", scenario.getName());
+        }
+
         Driver.closeDriver();
         System.out.println(":::(^_^) End of test execution (*_*):::");
     }
